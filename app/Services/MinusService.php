c61bb1f2ebc215d12ceab74081bb35d000dd661a
Ibrahim A A <ibrahim.devwork@gmail.com>
2023-06-02 06:33:57 -0700

    Test is completed

--- app/Services/MinusService.php
+++ app/Services/MinusService.php
@@ -4,7 +4,7 @@ namespace App\Services;
 
 class MinusService implements InterfaceCalculatorService {
 
-    public function calculate($firstNumber, $secondNumber)  : float {
+    public function calculate($firstNumber, $secondNumber) : float {
         return round((floatVal($firstNumber) - floatVal($secondNumber)), 2);
     }
 }
